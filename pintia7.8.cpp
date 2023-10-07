7108d8d524a3c520b7473cd80e8960de31ac5719
Qiaostw1 <58353286+tangyance@users.noreply.github.com>
2023-10-07 11:11:53 +0900

    Delete pintia7.8.cpp

--- pintia7.8.cpp
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-#include<math.h>
-int main(void){
-	float money,year,rate,interest;
-	scanf("%If%If%If",&money,&year,&rate);
-	interest=money*pow((1+rate),year)-money;
-	printf("interest = %.2f\n",interest);
-	return 0;
-}
