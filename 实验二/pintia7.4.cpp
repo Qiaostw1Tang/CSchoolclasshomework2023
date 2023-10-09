b2a3e237f1af019494ed22f2b7163bbe6579934e
Qiaostw1 <58353286+tangyance@users.noreply.github.com>
2023-10-10 00:24:19 +0900

    Add files via upload

---
+++ 实验二/pintia7.4.cpp
@@ -0,0 +1,34 @@
+#include <stdio.h>
+int main(void)
+{
+	int a,b;/*定义个变量*/
+    char c; /*定义字符变量*/
+    float price,discount,output; 
+    a=b=price=discount=output=0;
+	scanf("%d%d %c",&a,&b,&c);
+    switch(b){
+        
+        case 90:
+            price = 6.95;
+            break;
+        case 93:
+            price = 7.44;
+            break;
+        case 97:
+            price = 7.93;
+            break;
+        return 0;
+    }
+    switch(c) {
+case 'm':
+            discount = 0.05;
+            break;
+        case 'e':
+            discount = 0.03;
+            break;
+            return 0;
+    }
+	output = a * price * (1 - discount);
+    printf("%.2f\n",output);
+	return 0;
+}
