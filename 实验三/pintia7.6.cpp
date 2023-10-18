b0c7a19df2216b5465462e0b624bddbabdc26abc
Qiaostw1 <58353286+Qiaostw1Tang@users.noreply.github.com>
2023-10-18 22:34:31 +0900

    Add files via upload

---
+++ 实验三/pintia7.6.cpp
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+    int n,sum=0;
+    scanf("%d",&n);
+    while(n>0){
+    if(n%2!=0){
+    sum=sum+n;}
+    scanf("%d",&n);}
+    printf("%d",sum);
+    return 0;
+}
